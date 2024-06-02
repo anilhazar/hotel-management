17142b8adc2eca99c7ea0c8b6ac28b251496cdc0
anilhazar <alianilhazar@gmail.com>
2024-06-02 16:47:30 +0300

    Add SpecialRoomRepository Interface, SpecialRoomMapper and SpecialRoomRepositoryImpl Classes

---
+++ src/repository/SpecialRoomRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import room.SpecialRoom;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SpecialRoomRepository {
+    void save(SpecialRoom specialRoom);
+    void update(SpecialRoom specialRoom);
+    Optional<SpecialRoom> findById(Long id);
+    List<SpecialRoom> findAll();
+
+}
