4d1037009ad9461b7ad8f60b613df7c2211f81d3
anilhazar <alianilhazar@gmail.com>
2024-06-25 13:29:01 +0300

    Refactor ConnectionManager fields to exact match with the ddl script

--- src/database/ConnectionManager.java
+++ src/database/ConnectionManager.java
@@ -5,18 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static Connection connection = null;
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "password";
+    private static Connection connection;
+    private static final String USERNAME = "test";
+    private static final String PASSWORD = "test123";
 
-    private static final String DBNAME = "database";
+    private static final String DBNAME = "hotel_management";
 
     private static final String HOST = "localhost";
 
     private static final int PORT = 9999;
 
     private ConnectionManager() {}
-
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             try {
